08ea2a1faab5387168d48a0793cbf28872dc57bc
kurumayumac <junmomo6780@gmail.com>
2024-07-15 01:19:18 +0900

    hello world

--- Nihonn-Unity/Assets/Scripts/Example.cs
+++ Nihonn-Unity/Assets/Scripts/Example.cs
@@ -7,7 +7,7 @@ public class Example : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+      Debug.Log("Hello world!");
     }
 
     // Update is called once per frame
