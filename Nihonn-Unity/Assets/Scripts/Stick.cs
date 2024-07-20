d5a6d1ad71398dd5cff86713a4b787fa5ae2d3c1
kurumayumac <junmomo6780@gmail.com>
2024-07-20 13:42:00 +0900

    粘着

--- Nihonn-Unity/Assets/Scripts/Stick.cs
+++ Nihonn-Unity/Assets/Scripts/Stick.cs
@@ -20,6 +20,34 @@ public class StickToObject : MonoBehaviour
             // springJoint.frequency = 1f;
             // springJoint.breakForce = 100f;
             // springJoint.breakTorque = 50f;
-		}
+		}else if(other.gameObject.CompareTag("onigiri")){
+            Debug.Log("touch");
+            FixedJoint2D fixedJoint = gameObject.AddComponent<FixedJoint2D>();
+            fixedJoint.connectedBody = other.rigidbody;
+            fixedJoint.breakForce = 80f;//大きくするほど離れない
+            fixedJoint.breakTorque = 50f;//同上
+
+            // SpringJoint2D springJoint = gameObject.AddComponent<SpringJoint2D>();
+            // springJoint.connectedBody = other.rigidbody;
+
+            // springJoint.dampingRatio = 0.5f;
+            // springJoint.frequency = 1f;
+            // springJoint.breakForce = 100f;
+            // springJoint.breakTorque = 50f;
+        }else if(other.gameObject.CompareTag("natto")){
+            Debug.Log("touch");
+            FixedJoint2D fixedJoint = gameObject.AddComponent<FixedJoint2D>();
+            fixedJoint.connectedBody = other.rigidbody;
+            fixedJoint.breakForce = 80f;//大きくするほど離れない
+            fixedJoint.breakTorque = 50f;//同上
+
+            // SpringJoint2D springJoint = gameObject.AddComponent<SpringJoint2D>();
+            // springJoint.connectedBody = other.rigidbody;
+
+            // springJoint.dampingRatio = 0.5f;
+            // springJoint.frequency = 1f;
+            // springJoint.breakForce = 100f;
+            // springJoint.breakTorque = 50f;
+        }
     }
 }
